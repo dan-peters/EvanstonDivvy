bea4d16dcfbe8a92332aaae1a15e5e114501e930
dan-peters <44811436+dan-peters@users.noreply.github.com>
2019-02-28 10:02:09 -0600

    created file for divvy data download

---
+++ DivvyData.R
@@ -0,0 +1,10 @@
+# Download Divvy data 
+
+#chicago divvy data
+#2016 3,4: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2016_Q3Q4.zip
+#2017 1,2: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2017_Q1Q2.zip
+#2017 3,4: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2017_Q3Q4.zip
+#2018 1: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2018_Q1.zip
+#2018 2: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2018_Q2.zip
+#2018 3: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2018_Q3.zip
+#2018 4: https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2018_Q4.zip
